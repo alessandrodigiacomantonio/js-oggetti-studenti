d319685664c641dc7022310210ed0d551453bbbd
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-29 16:08:32 +0100

    Esercizio 1, fase 2

--- js/script.js
+++ js/script.js
@@ -6,6 +6,38 @@ $(document).ready(function(){
     'eta': 0,
   }
   for (var k in studente) {
-    console.log(studente[k]);
+    // alert(studente[k]);
   }
+
+  var elencoStudentiCredibili = [
+    {
+      'nome': 'jacopa',
+      'cognome': 'Tricheco',
+      'eta': 1,
+    },
+    {
+      'nome': 'Saro',
+      'cognome': 'Foca',
+      'eta': 2,
+    },
+    {
+      'nome': 'Giacoma',
+      'cognome': 'Pinguino',
+      'eta': 3,
+    },
+    {
+      'nome': 'Andreo',
+      'cognome': 'Moffetta',
+      'eta': -2,
+    },
+    {
+      'nome': 'Gertrudo',
+      'cognome': 'Vertebra',
+      'eta': -1,
+    },
+  ];
+  for (var i = 0; i < elencoStudentiCredibili.length; i++) {
+      alert( elencoStudentiCredibili[i].nome +' '+ elencoStudentiCredibili[i].cognome );
+  }
+
 });
