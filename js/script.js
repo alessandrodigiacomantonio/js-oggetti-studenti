8156a7ebaff01e842843b53b35eea0c2aa64db0e
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-29 16:18:49 +0100

    Esercizio 1, fase 3

--- js/script.js
+++ js/script.js
@@ -1,43 +1,58 @@
 $(document).ready(function(){
 
-  var studente = {
-    'nome': '',
-    'cognome': '',
-    'eta': 0,
-  }
-  for (var k in studente) {
-    // alert(studente[k]);
-  }
+  // // fase 1
+  // var studente = {
+  //   'nome': '',
+  //   'cognome': '',
+  //   'eta': 0,
+  // }
+  // for (var k in studente) {
+  //   alert(studente[k]);
+  // }
 
-  var elencoStudentiCredibili = [
-    {
-      'nome': 'jacopa',
-      'cognome': 'Tricheco',
-      'eta': 1,
-    },
-    {
-      'nome': 'Saro',
-      'cognome': 'Foca',
-      'eta': 2,
-    },
-    {
-      'nome': 'Giacoma',
-      'cognome': 'Pinguino',
-      'eta': 3,
-    },
-    {
-      'nome': 'Andreo',
-      'cognome': 'Moffetta',
-      'eta': -2,
-    },
-    {
-      'nome': 'Gertrudo',
-      'cognome': 'Vertebra',
-      'eta': -1,
-    },
-  ];
-  for (var i = 0; i < elencoStudentiCredibili.length; i++) {
-      alert( elencoStudentiCredibili[i].nome +' '+ elencoStudentiCredibili[i].cognome );
-  }
+  // // fase 2
+  // var elencoStudentiCredibili = [
+  //   {
+  //     'nome': 'jacopa',
+  //     'cognome': 'Tricheco',
+  //     'eta': 1,
+  //   },
+  //   {
+  //     'nome': 'Saro',
+  //     'cognome': 'Foca',
+  //     'eta': 2,
+  //   },
+  //   {
+  //     'nome': 'Giacoma',
+  //     'cognome': 'Pinguino',
+  //     'eta': 3,
+  //   },
+  //   {
+  //     'nome': 'Andreo',
+  //     'cognome': 'Moffetta',
+  //     'eta': -2,
+  //   },
+  //   {
+  //     'nome': 'Gertrudo',
+  //     'cognome': 'Vertebra',
+  //     'eta': -1,
+  //   },
+  // ];
+  // for (var i = 0; i < elencoStudentiCredibili.length; i++) {
+  //     alert( elencoStudentiCredibili[i].nome +' '+ elencoStudentiCredibili[i].cognome );
+  // }
+
+  // fase 3
+  var elencoStudenti = [];
+  var studente = {
+    nome: "",
+    cognome: '',
+    eta: 0,
+  };
+  studente.nome = prompt('Inserisci il nome dello studente da aggiungere');
+  studente.cognome = prompt('Scrivi il cognome dello studente da aggiungere');
+  studente.eta = prompt('Immetti l\'età dello studente da aggiungere');
+  elencoStudenti.push(studente);
+  console.log(elencoStudenti);
 
 });
