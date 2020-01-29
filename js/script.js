1c92c3a3b27cce14ac2a4b817d26304acb472542
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-29 15:54:47 +0100

    Esercizio 1, fase 1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
-$(document).ready(funciton(){
-  
+$(document).ready(function(){
+
+  var studente = {
+    'nome': '',
+    'cognome': '',
+    'eta': 0,
+  }
+  for (var k in studente) {
+    console.log(studente[k]);
+  }
 });
