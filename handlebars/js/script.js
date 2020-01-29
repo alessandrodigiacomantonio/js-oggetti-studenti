4aa664e15d8a05c95904ca243014a61ef011ff5a
alessandrodigiacomantonio <alessandrodigiacomantonio94@gmail.com>
2020-01-29 17:25:41 +0100

    Esercizio 2

--- handlebars/js/script.js
+++ handlebars/js/script.js
@@ -1,5 +1,24 @@
 $(document).ready(function(){
 
-  
-
+  $('button').on('click',
+  function() {
+    var fonte = document.getElementById("di_pelle_di__pollo").innerHTML;
+    var modello = Handlebars.compile(fonte);
+    var contesto = { messaggio: "", classe:"",};
+    contesto.messaggio = $('#fece_una_palla').val();
+    contesto.classe = 'a_galla_a_vedere';
+    $('#fece_una_palla').val('');
+    var html = modello(contesto);
+    $('#tutti_i_pesci_vennero').append(html);
+    setTimeout(
+      function() {
+        var fonte = document.getElementById("di_pelle_di__pollo").innerHTML;
+        var modello = Handlebars.compile(fonte);
+        var contesto = { messaggio: "", classe:"",};
+        contesto.messaggio = 'Elefante elegante';
+        contesto.classe = 'la_palla_di';
+        var html = modello(contesto);
+        $('#tutti_i_pesci_vennero').append(html);
+      },1*1000);
+  });
 });
